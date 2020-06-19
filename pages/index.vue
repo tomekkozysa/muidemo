8f7e31b6c097c6484617c0c3f54e34aa3f58fede
Tomasz Kozysa <tomek@15nine.com>
2020-06-19 14:53:09 +0100

    copy updates, hints toggle

--- pages/index.vue
+++ pages/index.vue
@@ -4,59 +4,68 @@
       <p class="demoblob">
 
         Hi, this is a sequencing demo using Tone.js and mui: created for Bob@Charanga. 
-        It's uses Tone.Sequence and Tone.AMSynth. User interface utilises my own library, mui:
+        It's uses Tone.Sequence and Tone.AMSynth. User interface utilises my own library, mui:.
 
 
 
       </p>
 
-      <p class="demoblob">
+      <p class="demoblob" v-if="showhints">
      
       Here is a quick guide on how to use it:
       Below are Tone Master controls, BPM, Volume, Swing:
       </p>
         <ToneMaster :bpm="40" :volume="-20" /> 
      
-      <p class="demoblob"></p>
-
-
-        
-        <Group label="Sequencer">
-            <sequencer v-if="amsynth" 
-            class="sequencer"
-            :in="amsynth" :duration="'32n'" :length="64" address="amsynth-sequencer" />
-        </Group>
-
-         <p class="demoblob">
-
-2. This is a Sequencer,
-
-Before you use it though, make sure the keyboard is "armed" first. Click the keyboard component, once highlighted it will listen to the keyboard events. 
-
-<strong> Use Z / X to change the octave. </strong> 
-
+      <p class="demoblob" v-if="showhints"></p>        
+      <Group label="Sequencer">
+          <sequencer v-if="amsynth" 
+          class="sequencer"
+          :in="amsynth" :duration="'32n'" :length="64" address="amsynth-sequencer" />
+      </Group>
+      <p class="demoblob" v-if="showhints">
+      2. This is a Sequencer, is a little bit quirky.  It starts playing as soon as you hit the PLAY button in Tone Master control.
       </p>
-      <p class="demoblob">
+      <p class="demoblob" v-if="showhints">
+        Before you use it, make sure the keyboard is "armed". Click the keyboard component, once highlighted it will listen to the actual keyboard events - the A,W,S,D etc... will have notes assigned now.
+        <strong> Use Z / X to change the octave. </strong> 
+      </p>
+      <p class="demoblob" v-if="showhints">
         
-Back to Sequencer, pick a mode/action, start with Record. With that mode selected, you can pick when a note is played, with armed keyboard you can record particular note.
+      Back to Sequencer, the way to use it, is through the MODE panel:<br />
 
+
+      1. Select Record. <br />
+      2. Select a cell in the grid, to determine when the note is played <br />
+      3. With keyboard component armed, use your actual keyboard to schedule a particular note.<br />
+      4. Other modes can misbehave, however Random one, will quickly select a bunch of random cells in sequencer, swict back to record and you can schedul multiple random notes:
+
+    
       </p>
 
 
 
       <AMSynth ref="amsynth" />
 
+ 
+   
 
-      <p class="demoblob">
+      <p class="demoblob" v-if="showhints">
         <strong> When interacting with the UI, you can use ALT or SHIFT key to affect sensitivity of a dial.</strong>
       </p>
-      <p class="demoblob">
+      <p class="demoblob" v-if="showhints">
 A little bit more about mui: – music interface library. SVG based, uses CSS vars as design tokens. Some components use vue slots to inject a bespoke SVG element like an extra circle in the SWING dial. 
       </p>
-      <p class="demoblob">
+      <p class="demoblob" v-if="showhints">
   Currently mui: under construction and kept as a private repo, the code for this demo can be found here:
 
 <a href="https://github.com/tomekkozysa/muidemo" > https://github.com/tomekkozysa/muidemo </a>
+
+      </p>
+      <p class="demoblob">
+
+             <mui-pad label="TOGGLE HINTS" mode="toggle" :w="80" :h="20" :padding="0"
+      @update="(e)=>{this.showhints = !this.showhints }"/>
 
       </p>
 
@@ -89,6 +98,7 @@ export default {
   data(){
       return{
             amsynth:null,
+            showhints: true,
             
       }
     
